8e547d102c2c1a62da752779a90e1e583c48813c
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-11-29 20:40:59 -0400

    test with done in async

--- src/base/08-imp-exp.js
+++ src/base/08-imp-exp.js
@@ -8,12 +8,12 @@ import heroes from '../data/heroes';
 
 
 
-export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
 // console.log( getHeroeById(2) );
 
 // find?, filter
-export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+export const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
 
 // console.log( getHeroesByOwner('Marvel') );
 
--- src/tests/base/09-promesas.test.js
+++ src/tests/base/09-promesas.test.js
@@ -1,28 +1,35 @@
-import getHeroeByIdAsync from "../../base/09-promesas"
+import { getHeroeByIdAsync } from '../../base/09-promesas'
 import heroes from '../../data/heroes'
 
+
+//TEST de funciones ASYNC
 describe('Pruebas con promesas', () => {
 
-    test('getHeroeByIdAsync debe de retornar un heroe async', (done) => {
+    //usamos el DONE para tareas async
+    test('debe de retornar un Heroe async', (done) => {
         const id = 1
 
         getHeroeByIdAsync(id)
             .then(heroe => {
-                expect(heroe).toBe(heroe[0])
+                //esperamos batman y se obtiene batman
+                expect(heroe).toBe(heroes[0])
+
+                //el done marca donde termina el test
                 done()
             })
     })
 
-
-    test('debe de obtener un error si el heroe por id no existe', (done) => {
+    test('debe de retornar un error si el heroe no existe', (done) => {
         const id = 10
 
         getHeroeByIdAsync(id)
             .catch(error => {
+                //esperamos un error que contiene el mensaje !!!!
                 expect(error).toBe('No se pudo encontrar el héroe')
+
+                //el done marca donde termina el test
                 done()
             })
     })
-
 
 })
