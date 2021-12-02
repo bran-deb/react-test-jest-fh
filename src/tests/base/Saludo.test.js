d8c76d5123cbf1488ad080956a6fa198517888cf
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-02 18:26:02 -0400

    test with enzyme

--- src/tests/base/Saludo.test.js
+++ src/tests/base/Saludo.test.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import { render } from "@testing-library/react"
+import '@testing-library/jest-dom'
+// import { render } from "@testing-library/react"
 import Saludo from "../../components/Saludo"
-
+import { shallow } from 'enzyme'
 
 describe('Pruebas en el componente <Saludo />', () => {
+    // test('debe de mostrar el mensaje "hola soy goku"', () => {
+    //     const mensaje = 'hola soy goku'
+    //     //render es una funcion que recibe un componente a renderizar
+    //     const { getByText } = render(<Saludo mensaje={mensaje} />)
+    //     //usamos destructuring para obtener el getbytext
+    //     expect(getByText(mensaje)).toBeInTheDocument()//toBeInTheDocument lo usa de setup
+    // })
 
-    test('debe de mostrar el mensaje "hola soy goku"', () => {
+    test('debe de demostrar <Saludo /> correctamente', () => {
 
-        const mensaje = 'hola soy goku'
+        const mensaje = 'Hola, soy Goku'
+        const wrapper = shallow(<Saludo mensaje={mensaje} />)
 
-        //render es una funcion que recibe un componente a renderizar
-        const { getByText } = render(<Saludo mensaje={mensaje} />)
-        //usamos destructuring para obtener el getbytext
-        expect(getByText(mensaje)).toBeInTheDocument()
+        expect(wrapper).toMatchSnapshot()
     })
 
 })
