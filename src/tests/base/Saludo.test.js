a5d905489fd59c38b67f4dbaff3a1bbe46328f26
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-02 18:49:53 -0400

    test from html

--- src/tests/base/Saludo.test.js
+++ src/tests/base/Saludo.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 // import { render } from "@testing-library/react"
 import Saludo from "../../components/Saludo"
 import { shallow } from 'enzyme'
+import { getByText } from '@testing-library/react'
 
 describe('Pruebas en el componente <Saludo />', () => {
     // test('debe de mostrar el mensaje "hola soy goku"', () => {
@@ -20,5 +21,25 @@ describe('Pruebas en el componente <Saludo />', () => {
 
         expect(wrapper).toMatchSnapshot()
     })
+
+    //pruebas para el html
+    test('debe de mostrar el subtitulo enviado por props', () => {
+
+        const mensaje = 'Hola, soy Goku'
+        const subtitulo = 'soy un subtitulo'
+        const wrapper = shallow(
+            <Saludo
+                mensaje={mensaje}
+                subtitulo={subtitulo}
+            />
+        )
+        // FIND funciona como el document.querySelector
+        const textoParrafo = wrapper.find('p').text()
+        //mostramos el texto del parrafo
+        console.log(textoParrafo)
+
+        expect(textoParrafo).toBe(subtitulo)
+    })
+
 
 })
