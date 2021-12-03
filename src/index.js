9d34eb644a363d8424ded31ccaa66021e7400faf
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-12-02 21:01:14 -0400

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import CounterApp from './components/CounterApp';
-import Saludo from './components/Saludo';
-
+import CounterApp from './components/CounterApp';
+// import Saludo from './components/Saludo';
 import './index.css'
 
 
 const divRoot = document.querySelector('#root')
 
-// ReactDOM.render(<CounterApp />, divRoot)
-ReactDOM.render(<Saludo mensaje='soy un mensaje' />, divRoot)
+ReactDOM.render(<CounterApp value={10} />, divRoot)
+// ReactDOM.render(<Saludo mensaje='soy un mensaje' />, divRoot)
